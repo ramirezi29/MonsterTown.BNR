f7386dcd136b962889010f7555132550444de7e2
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-08-12 12:04:08 -0600

    Initial Commit

---
+++ MonsterTown4.BNR/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown4.BNR
+//
+//  Created by Ivan Ramirez on 8/12/18.
+//  Copyright © 2018 BNR.ramcomw. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
